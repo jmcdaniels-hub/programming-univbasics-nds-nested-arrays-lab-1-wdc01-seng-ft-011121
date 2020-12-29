7666a94108608f1c26ad24da2abed3bba0d3d108
Jade McDaniels <jademcdaniels81@gmail.com>
2020-12-29 23:58:35 +0000

    Done.

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -17,6 +17,8 @@ CONVENTIONAL_PRODUCE = [
 ]
 
 def assembled_matrix
+  produce_storage_room = [CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
+  
   # Build an array that contains both of the above arrays
   # This matrix will represent a produce storage room
   # Organic standards require that organic products be stored ABOVE conventional, not the other way around
@@ -24,17 +26,33 @@ def assembled_matrix
 end
 
 def sorted_matrix
+  
+  produce_storage_room = [CONVENTIONAL_PRODUCE.sort, ORGANIC_PRODUCE.sort]
+  
   # Using Array literal syntax only, build another nested array that 
   # uses the arrays of conventional and organic produce as before.
   # However, this time, sort each internal array alphabetically by the first character
 end
 
-def matrix_lookup(matrix, row, column)
+def matrix_lookup(matrix= assembled_matrix, row= 1, column= 1)
+  matrix[row][column]
+  
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
 end
 
-def matrix_update(matrix, row, column, new_value)
+def matrix_update(matrix = local_copy_of_matrix,row = 0 , column = 2, new_value = "Lemons")
+  
+   matrix[row][column] = new_value
+   
+   result = matrix
+    
+  
+  
+  
+  
+  #result = matrix_update(local_copy_of_matrix, 0, 2, "Lemons")
+  #expect(result[0]).to eq(["Grapefruit", "Pineapple", "Lemons", "Watermelon", "Eggplant"])
   # Given any matrix (array of arrays), a row index and a column index, 
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix
